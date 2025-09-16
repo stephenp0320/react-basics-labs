8b2a52f938f1560d5cfcf3b6f2426d77ad3a9ede
stephenp0320 <stephen power>
2025-09-16 13:53:18 +0100

    tasky lab 1

---
+++ tasky/src/components/Task.jsx
@@ -0,0 +1,14 @@
+// src/components/Task.jsx
+import React from 'react';
+
+const Task = (props) => {
+  return (
+    <div className="card">
+      <p className="title">{props.title}</p>
+      <p>Due: {props.due}</p>
+      <p className="description">{props.description}</p>
+    </div>
+  );
+};
+
+export default Task;
